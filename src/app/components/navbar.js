9efb50c3f4a99aa24e15a3f20fe23c0d41b64f74
Yashpratap421 <yash1pratap2@gmail.com>
2025-07-12 12:40:21 +0530

    Added a Menu for Mobile View

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,36 +1,54 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import { IoIosSearch } from "react-icons/io";
-import { CiUser } from "react-icons/ci";
-import { CiBookmark } from "react-icons/ci";
+import { CiUser, CiBookmark } from "react-icons/ci";
 import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross2 } from "react-icons/rx";
 import Image from "next/image";
 import Link from 'next/link';
 import logo from "../../../public/Images/logo.svg";
 
-
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
-        <div className='nav-bar'>
-            <div className='nav-top'>
-                <div className='nav-logo'>
-                    <Image src={logo} alt="logo" className='logo' />
-                </div>
-                <div className='nav-icons'>
-                    <IoIosSearch className='icon'/>
-                    <CiUser className='icon'/>
-                    <CiBookmark className='icon'/>
-                    <HiOutlineShoppingBag className='icon'/>
-                </div>
-            </div>
-            <div className='nav-bottom'>
+        <>
+            <div className={`slider ${menuOpen ? "open" : ""}`}>
+                <RxCross2 className="close-icon" onClick={() => setMenuOpen(false)} />
                 <Link href={""} className='nav-links'>Bags</Link>
                 <Link href={""} className='nav-links'>Travel</Link>
                 <Link href={""} className='nav-links'>Accesories</Link>
                 <Link href={""} className='nav-links'>Gifting</Link>
                 <Link href={""} className='nav-links'>Jewelery</Link>
             </div>
-        </div>
-  )
+
+            <div className='nav-bar'>
+                <div className='nav-top'>
+                    <RxHamburgerMenu className="menu-icon" onClick={() => setMenuOpen(true)} />
+
+                    <div className='nav-logo'>
+                        <Image src={logo} alt="logo" className='logo' />
+                    </div>
+
+                    <div className='nav-icons'>
+                        <IoIosSearch className='icon'/>
+                        <CiUser className='icon'/>
+                        <CiBookmark className='icon'/>
+                        <HiOutlineShoppingBag className='icon'/>
+                    </div>
+                </div>
+
+                <div className='nav-bottom'>
+                    <Link href={""} className='nav-links'>Bags</Link>
+                    <Link href={""} className='nav-links'>Travel</Link>
+                    <Link href={""} className='nav-links'>Accesories</Link>
+                    <Link href={""} className='nav-links'>Gifting</Link>
+                    <Link href={""} className='nav-links'>Jewelery</Link>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
